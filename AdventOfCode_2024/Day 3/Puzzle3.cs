0c93a435625098a350bafcd10a2924248b47c302
BeratAhmetaj <beratahmetaj02@gmail.com>
2024-12-03 13:36:55 +0100

    Day 3

--- AdventOfCode_2024/Day 3/Puzzle3.cs
+++ AdventOfCode_2024/Day 3/Puzzle3.cs
@@ -1,10 +1,49 @@
-﻿namespace AdventOfCode_2024.Day_3
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode_2024.Day_3
 {
     public class Puzzle3
     {
         public static void Solution(string[] args)
         {
-            Console.WriteLine("Day 3 Puzzle 3");
+
+            //StreamRead the input
+            string[] lines = File.ReadAllLines("../../../Day 3/input.txt");
+
+            string pattern = @"mul\((\d+),(\d+)\)";
+            Regex regex = new Regex(pattern);
+
+
+            bool doFlag = true;
+            int total = 0;
+
+            foreach (var line in lines)
+            {
+                foreach (Match match in Regex.Matches(line, @"do\(\)|don't\(\)|mul\((\d+),(\d+)\)"))
+                {
+                    if(match.Value == "do()")
+                    {
+                        doFlag = true;
+                        Console.WriteLine($"'do()' found: Flag set to {doFlag}");
+
+                    }
+                    else if (match.Value == "don't()")
+                    {
+                        doFlag = false;
+                        Console.WriteLine($"'don't()' found: Flag set to {doFlag}");
+                    }
+
+                    if (doFlag && regex.IsMatch(match.Value) )
+                    {
+                        int num1 = int.Parse(match.Groups[1].Value);
+                        int num2 = int.Parse(match.Groups[2].Value);
+                        total += num1 * num2;
+                        Console.WriteLine(match.Value + " = " + num1 * num2);
+                    }
+
+                }
+            }
+            Console.WriteLine("Total: " + total);
         }
     }
 }
